f6ede5d9c4af7e2d1d112222bea94cb98aca1bde
Marshall Bowers <elliott.codes@gmail.com>
2024-07-05 14:46:28 -0400

    Respect `sourcekit-lsp` from $PATH

--- src/swift.rs
+++ src/swift.rs
@@ -10,8 +10,16 @@ impl zed::Extension for SwiftExtension {
     fn language_server_command(
         &mut self,
         _server_id: &zed::LanguageServerId,
-        _worktree: &zed::Worktree,
+        worktree: &zed::Worktree,
     ) -> Result<zed::Command> {
+        if let Some(path) = worktree.which("sourcekit-lsp") {
+            return Ok(zed::Command {
+                command: path,
+                args: Default::default(),
+                env: worktree.shell_env(),
+            });
+        }
+
         Ok(zed::Command {
             command: "/usr/bin/xcrun".into(),
             args: vec!["sourcekit-lsp".into()],
