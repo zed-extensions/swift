fbd4dae70211c26f6e1679a0f0b0a128c57ec9e6
Yaroslav Yashin <yaroslav.yashin@gmail.com>
2024-08-22 23:06:08 +0200

    Functions and Variables declaration added Code refactored

--- languages/swift/outline.scm
+++ languages/swift/outline.scm
@@ -1,31 +1,35 @@
 (protocol_declaration
-  declaration_kind: (_) @context
-  name: (type_identifier) @name) @item
-
-(class_declaration
-    declaration_kind: "struct" @context
+    declaration_kind: "protocol" @name
     name: (type_identifier) @name) @item
 
 (class_declaration
-    declaration_kind: "class" @context
+    declaration_kind: "struct" @name
     name: (type_identifier) @name) @item
 
 (class_declaration
-    declaration_kind: "extension" @context
+    declaration_kind: "class" @name
+    name: (type_identifier) @name) @item
+
+(class_declaration
+    declaration_kind: "extension" @name
     name: (user_type) @name) @item
 
 (class_declaration
-    declaration_kind: "actor" @context
-    name: (type_identifier) @name) @item @context.actor
+    declaration_kind: "actor" @name
+    name: (type_identifier) @name) @item ;@context.actor
 
 (init_declaration
-  name: "init" @name) @item
+    name: "init" @name) @item
 
 (deinit_declaration
-  "deinit" @name) @item
+    "deinit" @name) @item
 
 (function_declaration
-  name: (simple_identifier) @name) @item
+    "func" @name
+    name: (simple_identifier) @name
+    ) @item
 
 (property_declaration
-  name: (simple_identifier) @name) @item
+    (value_binding_pattern) @name
+    name: (pattern) @name
+) @item
