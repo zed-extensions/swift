5571747743ccd92156ecb499183ea4b24ae1627e
Cole Miller <cole@zed.dev>
2025-06-23 15:46:42 -0400

    wip

--- src/swift.rs
+++ src/swift.rs
@@ -70,6 +70,43 @@ impl zed::Extension for SwiftExtension {
             _ => None,
         }
     }
+
+    fn get_dap_binary(
+        &mut self,
+        _adapter_name: String,
+        _config: zed_extension_api::DebugTaskDefinition,
+        user_provider_debug_adapter_path: Option<String>,
+        worktree: &zed_extension_api::Worktree,
+    ) -> Result<zed_extension_api::DebugAdapterBinary, String> {
+        let command = if let Some(command) = user_provider_debug_adapter_path {
+            command
+        } else {
+            todo!()
+        };
+        Ok(zed_extension_api::DebugAdapterBinary {
+            command: Some(command),
+            arguments: Vec::new(),
+            envs: worktree.shell_env(),
+            cwd: Some(worktree.root_path()),
+            connection: None,
+            request_args: todo!(),
+        })
+    }
+
+    fn dap_request_kind(
+        &mut self,
+        _adapter_name: String,
+        _config: serde_json::Value,
+    ) -> Result<zed_extension_api::StartDebuggingRequestArgumentsRequest, String> {
+        todo!()
+    }
+
+    fn dap_config_to_scenario(
+        &mut self,
+        _config: zed_extension_api::DebugConfig,
+    ) -> Result<zed_extension_api::DebugScenario, String> {
+        todo!()
+    }
 }
 
 zed::register_extension!(SwiftExtension);
