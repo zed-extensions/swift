82cd1d5cc2da46a17778b4c70a9e8b80782786cf
Cole Miller <cole@zed.dev>
2025-06-24 00:17:24 -0400

    wip

--- src/swift.rs
+++ src/swift.rs
@@ -1,6 +1,10 @@
 mod language_server;
+
+use std::collections::HashMap;
+
 use language_server::SourceKitLsp;
 
+use serde::{Deserialize, Serialize};
 use zed::settings::LspSettings;
 use zed_extension_api::{
     self as zed,
@@ -11,6 +15,15 @@ use zed_extension_api::{
 #[derive(Default)]
 struct SwiftExtension {
     sourcekit_lsp: Option<SourceKitLsp>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct SwiftDebugConfig {
+    #[serde(default)]
+    cwd: Option<String>,
+    #[serde(default)]
+    env: HashMap<String, String>,
+    program: String,
 }
 
 impl zed::Extension for SwiftExtension {
@@ -74,38 +87,75 @@ impl zed::Extension for SwiftExtension {
     fn get_dap_binary(
         &mut self,
         _adapter_name: String,
-        _config: zed_extension_api::DebugTaskDefinition,
-        user_provider_debug_adapter_path: Option<String>,
+        config: zed_extension_api::DebugTaskDefinition,
+        user_provided_debug_adapter_path: Option<String>,
         worktree: &zed_extension_api::Worktree,
     ) -> Result<zed_extension_api::DebugAdapterBinary, String> {
-        let command = if let Some(command) = user_provider_debug_adapter_path {
-            command
-        } else {
-            todo!()
-        };
+        let configuration = config.config.to_string();
+        let config: SwiftDebugConfig =
+            serde_json::from_str(&config.config).map_err(|e| e.to_string())?;
+        let command = user_provided_debug_adapter_path
+            .or_else(|| {
+                worktree.which("/Applications/Xcode.app/Contents/Developer/usr/bin/lldb-dap")
+            })
+            .or_else(|| worktree.which("/Library/Developer/CommandLineTools/usr/bin/lldb-dap"))
+            .or_else(|| worktree.which("lldb-dap"))
+            .ok_or_else(|| "Could not find lldb-dap".to_owned())?;
         Ok(zed_extension_api::DebugAdapterBinary {
             command: Some(command),
             arguments: Vec::new(),
-            envs: worktree.shell_env(),
-            cwd: Some(worktree.root_path()),
+            envs: config.env.into_iter().collect(),
+            cwd: Some(config.cwd.unwrap_or_else(|| worktree.root_path())),
             connection: None,
-            request_args: todo!(),
+            request_args: zed_extension_api::StartDebuggingRequestArguments {
+                configuration,
+                request: todo!(),
+            },
         })
     }
 
     fn dap_request_kind(
         &mut self,
         _adapter_name: String,
-        _config: serde_json::Value,
+        config: serde_json::Value,
     ) -> Result<zed_extension_api::StartDebuggingRequestArgumentsRequest, String> {
-        todo!()
+        match config.get("request") {
+            Some(launch) if launch == "launch" => {
+               Ok(zed_extension_api::StartDebuggingRequestArgumentsRequest::Launch)
+            }
+            Some(attach) if attach == "attach" => {
+                Ok(zed_extension_api::StartDebuggingRequestArgumentsRequest::Attach)
+            }
+            Some(value) => Err(format!("Unexpected value for `request` key in Swift debug adapter configuration: {value:?}")),
+            None => {
+                Err("Missing required `request` field in Swift debug adapter configuration".into())
+            }
+        }
     }
 
     fn dap_config_to_scenario(
         &mut self,
-        _config: zed_extension_api::DebugConfig,
+        zed_scenario: zed_extension_api::DebugConfig,
     ) -> Result<zed_extension_api::DebugScenario, String> {
-        todo!()
+        match zed_scenario.request {
+            zed_extension_api::DebugRequest::Launch(launch) => {
+                let config = serde_json::to_string(&SwiftDebugConfig {
+                    program: launch.program,
+                    env: launch.envs.into_iter().collect(),
+                    cwd: launch.cwd.clone(),
+                })
+                .unwrap();
+
+                Ok(zed_extension_api::DebugScenario {
+                    adapter: zed_scenario.adapter,
+                    label: zed_scenario.label,
+                    config,
+                    tcp_connection: None,
+                    build: None,
+                })
+            }
+            zed_extension_api::DebugRequest::Attach(_) => todo!(),
+        }
     }
 }
 
