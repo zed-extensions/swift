3aa55c97c04d5d33503bacdb1e6d895403270970
Donnie Adams <donnie@thedadams.com>
2025-10-22 20:04:01 -0400

    chore: add comment language injection
    Signed-off-by: Donnie Adams <donnie@thedadams.com>

--- languages/swift/injections.scm
+++ languages/swift/injections.scm
@@ -2,3 +2,9 @@
 
 ((regex_literal) @injection.content
  (#set! injection.language "regex"))
+
+((comment) @injection.content
+ (#set! injection.language "comment"))
+
+((multiline_comment) @injection.content
+ (#set! injection.language "comment"))
