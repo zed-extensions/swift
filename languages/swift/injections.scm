4921a6f05930d278c735cb4a24ae6f0e35d827de
Donnie Adams <donald.g.adams@me.com>
2025-10-28 06:09:03 -0400

    Add comment language injection (#43)
    Addresses https://github.com/thedadams/zed-comment/issues/14
    
    ---------
    
    Signed-off-by: Donnie Adams <donnie@thedadams.com>
    Co-authored-by: MrSubidubi <dev@bahn.sh>

--- languages/swift/injections.scm
+++ languages/swift/injections.scm
@@ -2,3 +2,11 @@
 
 ((regex_literal) @injection.content
  (#set! injection.language "regex"))
+
+(
+  [
+    (comment)
+    (multiline_comment)
+  ] @injection.content
+ (#set! injection.language "comment")
+)
