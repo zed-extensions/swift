936bad5c5ba1043ddeb097ccbb79f7549f6dc1a9
MrSubidubi <dev@bahn.sh>
2025-10-28 11:08:39 +0100

    Combine injections

--- languages/swift/injections.scm
+++ languages/swift/injections.scm
@@ -3,8 +3,10 @@
 ((regex_literal) @injection.content
  (#set! injection.language "regex"))
 
-((comment) @injection.content
- (#set! injection.language "comment"))
-
-((multiline_comment) @injection.content
- (#set! injection.language "comment"))
+(
+  [
+    (comment)
+    (multiline_comment)
+  ] @injection.content
+ (#set! injection.language "comment")
+)
